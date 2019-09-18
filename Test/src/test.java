5bee75046fbf89f98ab88911e4aa76cc3e404d19
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2019-09-18 20:56:20 +0930

    Stuff

--- Test/src/test.java
+++ Test/src/test.java
@@ -3,9 +3,10 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test hello this is me");
+System.out.println("test hello this is me hello world this is me");
 int i = 0;
 int j = 1;
+int k = 1;
 	}
 
 }
