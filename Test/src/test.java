69b531b0078d801a2254a3bc25e34028f2485aca
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2019-09-19 20:30:57 +0930

    This is a commit

--- Test/src/test.java
+++ Test/src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test hello this is me hello world this is me");
+System.out.println("test hello this is me hello world this is me hello");
 int i = 0;
 int j = 1;
 int k = 1;
