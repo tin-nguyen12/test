9ffb5a68aff1fac824bbc84c7fcecd85ee3317bb
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2019-09-23 13:56:42 +0930

    # WARNING: head commit changed in the meantime
    sdfsdf

--- Test/src/test.java
+++ Test/src/test.java
@@ -4,14 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test hello this is me hello world this is me hello 2");
-		int i = 0;
-		int j = 1;
-		int k = 1;
-		int o = 1;
-		float g = 2.0;
-		String words = "hello";
-		int asda = 2;
-		int asdas = 10;
+		System.out.println("new branch");
 	}
 
 }
